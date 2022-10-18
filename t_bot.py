7e8ff169fd9294029118a933ae2a30727b0805e6
Zeler91 <51795219+Zeler91@users.noreply.github.com>
2022-10-19 01:22:07 +0300

    init webhook

--- t_bot.py
+++ t_bot.py
@@ -3,7 +3,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 from ali_resione_parser import search_product_by_attributes
 
-T_TOKEN = '5790801631:AAF1rk_MZJH5bBhRV5Rn3nS-s4z9i7mAemI'
+TOKEN = '5790801631:AAF1rk_MZJH5bBhRV5Rn3nS-s4z9i7mAemI'
 
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -14,12 +14,15 @@ async def start_search(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     resione = search_product_by_attributes()
     await update.message.reply_text(f'{resione["title"]} \n{resione["price"]} \n{ resione["url"]}')
 
-app = ApplicationBuilder().token(T_TOKEN).build()
+app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("hello", hello))
 app.add_handler(CommandHandler("search", start_search))
 
-app.run_polling()
+app.run_webhook(listen='0.0.0.0',
+    port=8443,
+    url_path='TOKEN',
+    webhook_url='https://api.render.com/deploy/srv-cd7i3o2en0hgupusmas0?key=FYfTJtPiyRY/' + TOKEN)
 
 
 
