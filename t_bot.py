4df0272cb02f299f359438dad0e77f84c4c2bc93
Zeler91 <51795219+Zeler91@users.noreply.github.com>
2022-10-21 01:14:48 +0300

    replace webhook from pooling

--- t_bot.py
+++ t_bot.py
@@ -19,51 +19,10 @@ app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("hello", hello))
 app.add_handler(CommandHandler("search", start_search))
 
-app.run_webhook(listen='0.0.0.0',
-    port=8443,
-    url_path='TOKEN',
-    webhook_url='https://api.render.com/deploy/srv-cd7i3o2en0hgupusmas0?key=FYfTJtPiyRY/' + TOKEN)
+# app.run_webhook(listen='0.0.0.0',
+#     port=8443,
+#     url_path='TOKEN',
+#     webhook_url='https://api.render.com/deploy/srv-cd7i3o2en0hgupusmas0?key=FYfTJtPiyRY/' + TOKEN)
 
-
-
-
-
-
-
-# # Update - кусок информации полученный нами от Телеграма
-# from telegram import Update
-
-# # ApplicationBuilder - способ создать приложение с указанием настроек
-# from telegram.ext import ApplicationBuilder, MessageHandler, filters
-
-# from head_hunter import parse_hh
-# from sectoken import TOKEN
-# # token - секретный ключ к вашему боту (получить у @BotFather)
-# app = ApplicationBuilder().token(TOKEN).build()
-
-
-# # Функция вызывается при получении сообщения
-# # upd - новая информация от ТГ
-# # ctx - служебная информация
-# async def text_reply(upd: Update, _ctx):
-#     user_text = upd.message.text
-#     print(f"User: {user_text}")
-#     # Запустить парсинг
-#     name = upd.message.from_user.full_name
-#     reply = f"Уважаемый {name}, мы получили ваш запрос '{user_text}'"
-#     print(reply)
-#     await upd.message.reply_text(reply)
-#     jobs_count = parse_hh(user_text)
-#     await upd.message.reply_text(f"Найдено вакансий: {jobs_count}")
-
-
-# # Handler - обработчик сообщения
-# handler = MessageHandler(filters.TEXT, text_reply)
-# # MessageHandler - для сообщений (текстовые, аудио, анимации, стикеры...)
-# # CommandHandler - для команд (типа /hello /start )
-
-# # Прикрепляем обработчик к приложению
-# app.add_handler(handler)
-
-# # Запускаем приложение
-# app.run_polling()
+# Запускаем приложение
+app.run_polling()
