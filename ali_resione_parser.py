380f16b1ef9437f223acc014817f0e6ba9d024f3
Zeler91 <51795219+Zeler91@users.noreply.github.com>
2022-10-23 00:10:56 +0300

    chrome options added

--- ali_resione_parser.py
+++ ali_resione_parser.py
@@ -10,7 +10,10 @@ SEARCH_REQUEST = 'Resione m68'
 
 def init_browser():
     options = Options()
-    options.headless = True
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--headless')
+    options.add_argument('--disable-dev-shm-usage')
     browser = webdriver.Chrome(options=options)
     return browser
 
