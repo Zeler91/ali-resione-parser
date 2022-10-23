1daed33244d3af522ace6b658a9e4a07d7ac6a62
Zeler91 <51795219+Zeler91@users.noreply.github.com>
2022-10-23 21:22:45 +0300

    add window arg to chrome

--- ali_resione_parser.py
+++ ali_resione_parser.py
@@ -13,7 +13,9 @@ def init_browser():
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-gpu')
     options.add_argument('--headless')
+    options.add_argument("--window-size=1920,1080")
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-extensions')
     browser = webdriver.Chrome(options=options)
     return browser
 
