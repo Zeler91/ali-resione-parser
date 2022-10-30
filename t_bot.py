3859fd4c64d252cd044ef148cc2d88c411379edc
Zeler91 <51795219+Zeler91@users.noreply.github.com>
2022-10-30 22:48:13 +0300

    add env file

--- t_bot.py
+++ t_bot.py
@@ -4,12 +4,8 @@ import os
 from ali_resione_parser import search_product_by_attributes
 from dotenv import load_dotenv
 
-# token for local
 load_dotenv()
-TOKEN_LOCAL = os.getenv('BOT_TOKEN')
-
-# token env for server
-TOKEN = os.environ.get('BOT_TOKEN', TOKEN_LOCAL)
+TOKEN = os.getenv('BOT_TOKEN')
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_name = update.effective_user.full_name
@@ -24,10 +20,4 @@ app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("hello", hello))
 app.add_handler(CommandHandler("search", start_search))
 
-# app.run_webhook(listen='0.0.0.0',
-#     port=8443,
-#     url_path='TOKEN',
-#     webhook_url='https://api.render.com/deploy/srv-cd7i3o2en0hgupusmas0?key=FYfTJtPiyRY/' + TOKEN)
-
-# Запускаем приложение
 app.run_polling()
