4c11ea7cafd8bc82c0207af315645b50c345a827
Zeler91 <51795219+Zeler91@users.noreply.github.com>
2022-11-02 01:06:27 +0300

    modified ali_resione_parser.py

--- ali_resione_parser.py
+++ ali_resione_parser.py
@@ -1,25 +1,26 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
-
+# import chromedriver_binary
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 SEARCH_REQUEST = 'Resione m68'
 
-def init_browser():
-    options = Options()
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--headless')
-    options.add_argument("--window-size=1920,1080")
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--disable-extensions')
-    browser = webdriver.Chrome(options=options)
-    browser.implicitly_wait(1)
-    return browser
 
-def input_search_request(search_request:str, browser:webdriver):
+options = Options()
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-gpu')
+options.add_argument('--headless')
+options.add_argument("--window-size=1920,1080")
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--disable-extensions')
+browser = webdriver.Chrome(options=options)
+browser.implicitly_wait(1)
+
+
+def input_search_request(search_request:str):
+    global browser
     browser.get("https://www.aliexpress.ru")
     search_input = browser.find_element(By.ID, "searchInput")  
     search_input.send_keys(search_request) 
@@ -30,40 +31,39 @@ def sort_elements_by_price(elements_list):
     price_list_sorted = []
     elements_list_sorted = []
     for element in elements_list:
-        price_list_sorted.append(element.get_attribute("innerHTML"))
+        price_list_sorted.append(element.text)
     price_list_sorted.sort()
     for price in price_list_sorted:
         for element in elements_list:
-            if price == element.get_attribute("innerHTML"):
+            if price == element.text:
                 elements_list_sorted.append(element)
                 break
     return elements_list_sorted
 
-def find_product_on_item_page(browser:webdriver, product_type, product_weight_in_gramms):
-    browser.implicitly_wait(1)
+def find_product_on_item_page(*args):
+    global browser
     product_title = browser.find_element(By.XPATH, '/html/body/div[1]/div/div[8]/div[2]/div/div/div[1]/div/div[1]/div[1]/div/span[2]')
     product_price = browser.find_element(By.XPATH, '/html/body/div[1]/div/div[8]/div[2]/div/div/div[3]/div[1]/div/div[2]/div[2]')
-    products = browser.find_element(By.XPATH, '/html/body/div[1]/div/div[8]/div[2]/div/div/div[1]/div/div[1]/div[2]/div')
-    products_list = products.find_elements(By.TAG_NAME, 'div')
-    weight_in_kg = product_weight_in_gramms / 1000
-    product_index = 1
-    while product_index < len(products_list):
-        product_title_text = product_title.get_attribute("innerHTML")
-        if (str(product_weight_in_gramms) or str(weight_in_kg)) in product_title_text and product_type in product_title_text:
-            product_data = {'title' : product_title_text, 'price' : product_price.get_attribute('innerHTML').replace('&nbsp;', ''), 'url' : browser.current_url}
+    product_options = browser.find_element(By.XPATH, '/html/body/div[1]/div/div[8]/div[2]/div/div/div[1]/div/div[1]/div[2]/div')
+    product_options_list = product_options.find_elements(By.TAG_NAME, 'div')
+    assert(product_options_list, f'No product options')
+    weight_in_kg = args[1] / 1000
+    product_option_index = 1
+    while product_option_index < len(product_options_list):
+        if (str(args[1]) or str(weight_in_kg)) in product_title.text and args[0] in product_title.text:
+            product_data = {'title' : product_title.text, 'price' : product_price.text, 'url' : browser.current_url}
             return product_data
         else:
-            products_list[product_index].click()
-            browser.implicitly_wait(1)
+            product_options_list[product_option_index].click()
             product_title = browser.find_element(By.XPATH, '/html/body/div[1]/div/div[8]/div[2]/div/div/div[1]/div/div[1]/div[1]/div/span[2]')
-            product_price = browser.find_element(By.XPATH, '/html/body/div[1]/div/div[8]/div[2]/div/div/div[3]/div[1]/div/div[2]/div[2]')            
-            product_index += 1
+            product_price = browser.find_element(By.XPATH, '/html/body/div[1]/div/div[8]/div[2]/div/div/div[3]/div[1]/div/div[2]/div[2]')         
+            product_option_index += 1
     return None
 
 def search_product_by_attributes(product_type='M68', product_weight_in_gramms=1000):
-    browser = init_browser()
-    input_search_request(SEARCH_REQUEST, browser)
-    first_element_price = browser.find_element(By.XPATH, '/html/body/div[1]/div/div[4]/div[2]/div[2]/div[2]/div/div/div[1]/div/div/a/div[3]/div[2]/div[1]')
+    global browser
+    input_search_request(SEARCH_REQUEST)
+    first_element_price = browser.find_element(By.XPATH,'/html/body/div[1]/div/div[4]/div[2]/div[2]/div[2]/div/div/div[1]/div/div/a/div[3]/div[2]/div[1]')
     searched_elements_list = browser.find_elements(By.CLASS_NAME, first_element_price.get_attribute('class'))
     products_list = sort_elements_by_price(searched_elements_list)
     window_index = 1
@@ -71,7 +71,7 @@ def search_product_by_attributes(product_type='M68', product_weight_in_gramms=10
         product.click()
         browser.implicitly_wait(1)     
         browser.switch_to.window(browser.window_handles[window_index])
-        product = find_product_on_item_page(browser, product_type=product_type, product_weight_in_gramms=product_weight_in_gramms)
+        product = find_product_on_item_page(product_type, product_weight_in_gramms)
         if product:
             browser.quit()             
             return product
